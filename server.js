55aa3a8855069b2130b1683dece7c1bf2eaa0bc3
Adamya Kumar <kumar.adamya2000@gmail.com>
2022-03-25 02:45:58 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-const express =require('express')
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
 
-const app= express();
+//connect database
+connectDB();
 
-app.get('/',(req,res) => res.send('API Running'));
-const PORT =process.env.PORT || 5000;
+//Init Middleware
+app.use(express.json({ extended: false }));
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+app.get("/", (req, res) => res.send("API Running"));
+
+//define routes
+app.use("/api/users", require("./routes/API/users"));
+app.use("/api/auth", require("./routes/API/auth"));
+app.use("/api/profile", require("./routes/API/profile"));
+app.use("/api/posts", require("./routes/API/posts"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
